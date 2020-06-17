527959c0b0cc0810892ab3d222aa44e1a2fb352c
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2020-06-17 22:13:40 +0100

    Added initial machine implementation

--- turnstile-test.py
+++ turnstile-test.py
@@ -1,17 +1,31 @@
 import unittest
-
-from turnstile import transition
+from turnstile import StateMachine
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        states = ["Locked", "Unlocked"]
+        self.state_machine = StateMachine(
+            states=states,
+            transitions=["pass", "coin"],
+            terminals=["coin", "pass"],
+            initial_state=states[0]
+        )
+
     def test_framework_is_working(self):
         self.assertEqual(True, True)
 
     def test_turnstile_exists(self):
-        self.assertTrue(hasattr(transition, '__call__'))
+        self.assertIsInstance(self.state_machine, StateMachine)
 
     def test_initial_state_is_locked(self):
-        self.assertEqual("Locked", transition())
+        self.assertEqual("Locked", self.state_machine.initial_state)
+
+    def test_initial_state_changes_when_given_a_pass(self):
+        self.state_machine.transition("pass")
+        self.assertEqual("Unlocked", self.state_machine.current_state)
+
 
 if __name__ == '__main__':
     unittest.main()
--- turnstile.py
+++ turnstile.py
@@ -1,2 +1,16 @@
-def transition():
-    return "Locked"
+class StateMachine:
+
+    def __init__(self, states=None, transitions=None, terminals=None, initial_state=None):
+        self.states = states
+        self.transitions = transitions
+        self.terminals = terminals
+        self.initial_state = initial_state
+        self.current_state = initial_state
+
+    def transition(self, action):
+        try:
+            query_index = self.states.index(self.current_state)
+            if self.transitions[query_index] == action:
+                self.current_state = self.states[query_index + 1]
+        except Exception as e:
+            print(e)
